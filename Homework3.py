cef961befb5d00e2707d745eb550574c4cc853f4
SHvediSH <74442255+SHvediSH@users.noreply.github.com>
2024-04-09 07:11:59 +1000

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+my_string = input("fisting is?: ")
+print(my_string .replace("Fisting", "anal"))
+print(my_string .replace(" ", ""))
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string[0])
+print(my_string[-1])
